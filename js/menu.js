456f51e5524008de886a6a4de2aafbe85c0187ef
Luis A Sandoval <lasg.code@gmail.com>
2023-03-27 17:05:38 -0400

    Update navbar

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,12 @@
 const navIcon = document.querySelector(".nav-icon")
+const icons = document.querySelectorAll(".nav-icon i")
 const menu = document.querySelector(".navbar ul")
+const sections = document.querySelectorAll(".navbar ul li")
 
 navIcon.addEventListener("click", showMenu);
-menu.addEventListener("click", showMenu);
+sections.forEach(section => section.addEventListener("click", showMenu));
 
 function showMenu() {
     menu.classList.toggle("show");
+    icons.forEach(icon => icon.classList.toggle("hide"))
 }
