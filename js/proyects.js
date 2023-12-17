8ef5e5daf2cf6987db4a91a91c4e95475598a9d0
Luis A Sandoval <lasg.code@gmail.com>
2023-12-17 11:46:54 -0400

    update projects section

--- js/proyects.js
+++ js/proyects.js
@@ -3,63 +3,63 @@ const projects = [
         name: "Beatrice: Chatbot for English Practice",
         subtitle: "WebApp with chatbot",
         preview: "p4.png",
-        url: "https://github.com/SindromeDeLuis/betricebot",
-        url2: "https://beatricebot.onrender.com/",
+        github: "https://github.com/SindromeDeLuis/betricebot",
+        demo: "https://beatricebot.onrender.com/",
         technologies: ["HTML", "CSS", "Nodejs", "Expressjs", "MongoDB", "Dialogflow ES"],
     },
     p3 = {
         name: "Gestor de Trabajos de Grado para la Universidad José Antonio Páez",
         subtitle: "WebApp",
         preview: "p5.png",
-        url: "https://github.com/SindromeDeLuis/gestor-tesis-ujap",
-        url2: "https://www.flipsnack.com/BEBBD966AED/new-flipbook.html",
+        github: "https://github.com/SindromeDeLuis/gestor-tesis-ujap",
+        demo: "https://www.flipsnack.com/BEBBD966AED/new-flipbook.html",
         technologies: ["PHP", "Laravel", "Vuejs", "Bootstrap", "MySQL"],
     },
     p4 = {
         name: "E-commerce template",
         subtitle: "E-commerce",
         preview: "p2.png",
-        url: "https://github.com/SindromeDeLuis/E-commerce",
-        url2: "https://sindromedeluis.github.io/E-commerce/",
+        github: "https://github.com/SindromeDeLuis/E-commerce",
+        demo: "https://sindromedeluis.github.io/E-commerce/",
         technologies: ["HTML", "CSS", "JavaScript"],
     },
     p5 = {
         name: "Club de Tae Kwon Do Valle de San Diego",
         subtitle: "Website",
         preview: "p1.png",
-        url: "https://github.com/SindromeDeLuis/tae-kwon-do-san-diego",
-        url2: "https://sindromedeluis.github.io/tae-kwon-do-san-diego/",
+        github: "https://github.com/SindromeDeLuis/tae-kwon-do-san-diego",
+        demo: "https://sindromedeluis.github.io/tae-kwon-do-san-diego/",
         technologies: ["HTML", "CSS", "JavaScript"],
     },
     p6 = {
         name: "News/blog template",
         subtitle: "Website",
         preview: "p3.png",
-        url: "https://github.com/SindromeDeLuis/news-blog",
-        url2: "https://sindromedeluis.github.io/news-blog/",
+        github: "https://github.com/SindromeDeLuis/news-blog",
+        demo: "https://sindromedeluis.github.io/news-blog/",
         technologies: ["HTML", "CSS", "JavaScript"],
     },
 ]
 
-const projectList = document.querySelector("#gallery")
+const projectList = document.querySelector("#projects-gallery")
 const projectTemplate = document.querySelector("#project-template")
 const projectFragment = document.createDocumentFragment()
 
 projects.forEach(project => {
     const clone = projectTemplate.content.firstElementChild.cloneNode(true)
 
-    clone.querySelector("#name-project").textContent = project.name
-    clone.querySelector("#subtitle").textContent = project.subtitle
-    clone.querySelector("#image").src = "images/projects/"+project.preview
-    clone.querySelector("#image").loading = "lazy"
+    clone.querySelector("#project-name").textContent = project.name
+    clone.querySelector("#project-subtitle").textContent = project.subtitle
+    clone.querySelector("#project-image").src = "images/projects/"+project.preview
+    clone.querySelector("#project-image").alt = project.name
     project.technologies.forEach(technology => {
         let li = document.createElement("li")
         li.appendChild(document.createTextNode(technology))
         li.setAttribute("id", technology.toLowerCase())
-        clone.querySelector("#technologies").appendChild(li)
+        clone.querySelector("#project-technologies").appendChild(li)
     })
-    clone.querySelector("#url").setAttribute("href", project.url)
-    clone.querySelector("#url2").setAttribute("href", project.url2)
+    clone.querySelector("#url").setAttribute("href", project.github)
+    clone.querySelector("#url2").setAttribute("href", project.demo)
     
     projectFragment.appendChild(clone)
 })
